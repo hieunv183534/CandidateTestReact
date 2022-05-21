efe155f84e9e3894e13e0b0f81c19e9c9000f71d
hieunv183534 <hieu.nv183534@sis.hust.edu.vn>
2022-05-21 17:31:59 +0700

    menu

--- src/component/common/Menu.js
+++ src/component/common/Menu.js
@@ -1,45 +1,53 @@
-import React from "react";
+import React , {useState } from "react";
 import '../../css/common/Menu.css'
 
 import { Link } from 'react-router-dom'
 
 function Menu() {
+
+    const [optionActive, setOptionActive] = useState('/admin/');
+
+    const listMenu = [
+        {
+            to: '/admin/',
+            icon: 'home',
+            text: 'Trang chủ'
+        },
+        {
+            to: '/admin/account',
+            icon: 'user-gear',
+            text: 'Quản lí người dùng'
+        },
+        {
+            to: '/admin/question',
+            icon: 'clipboard-question',
+            text: 'Quản lí câu hỏi'
+        },
+        {
+            to: '/admin/test',
+            icon: 'circle-check',
+            text: 'Quản lí bài thi'
+        },
+        {
+            to: '/admin/testresult',
+            icon: 'bullseye',
+            text: 'Quản lí kết quả'
+        }
+    ]
+
     return (
         <div className="menu">
             <div className="menu-header">
             </div>
-            <Link to="/admin/">
-                <div className="menu-option">
-                    <i className="fas fa-home"></i>
-                    <p>Trang chủ</p>
-                </div>
-            </Link>
-            <Link to="/admin/account">
-                <div className="menu-option">
-                    <i className="fas fa-user-gear"></i>
-                    <p>Quản lí người dùng</p>
-                </div>
-            </Link>
-            <Link to="/admin/question">
-                <div className="menu-option">
-                <i className="fas fa-clipboard-question"></i>
-                    <p>Quản lí câu hỏi</p>
-                </div>
-            </Link>
-            <Link to="/admin/test">
-                <div className="menu-option">
-                <i className="fas fa-file-circle-check"></i>
-                    <p>Quản lí bài thi</p>
-                </div>
-            </Link>
-            <Link to="/admin/testresult">
-                <div className="menu-option">
-                <i className="fas fa-bullseye"></i>
-                    <p>Quản lí kết quả</p>
-                </div>
-            </Link>
+            {listMenu.map(item =>
+                <Link to={item.to} onClick={()=> setOptionActive(item.to)} key={item.to}>
+                    <div className={`menu-option ${item.to === optionActive ? 'active' : ''}`}>
+                        <i className={`fas fa-${item.icon}`}></i>
+                        <p>{item.text}</p>
+                    </div>
+                </Link>)}
         </div>
     );
-}
+};
 
 export default Menu;
