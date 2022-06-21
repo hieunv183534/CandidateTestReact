48c0f1681a3af1b511e663010c670e5f9319a254
DESKTOP-N4F2CA1\Admin <endee1106@gmail.com>
2022-06-21 22:55:58 +0700

    config TestPage

--- src/component/content/TestsContent.js
+++ src/component/content/TestsContent.js
@@ -1,32 +1,51 @@
 import React , { useState} from "react";
 import Table from "../common/Table";
 import Button from "../common/Button";
+
 import '../../css/layout/TestContent.css'
 
-
+var user = [{
+  name : 'John',
+  location : 'New York',
+  age : 21
+},
+{
+name : 'Andy',
+location : 'Bangkok',
+age : 22
+},
+{
+name : 'Lena',
+location : 'Pakistan',
+age : 18
+},
+{
+name : 'Luna',
+location : 'Taiwan',
+age : 19
+}]
 
 
 function TestsContent(){
     
     return( 
       <div className="TestContent">
-      <h1 className="title">Quản lý bài thi</h1>
-        <div className="questions">
-            <div className="questions-content">
-              <h2>Question 1</h2>
+        <h1 className="title">Quản lý bài thi</h1>
+       <div className="questions">
+        <div>
+         {user.map((user, index) => {
+          return (
+            <div key={index}  className="question-content">
+              {index}
+              <p>{user.name}</p>
+              <p>{user.location}</p>
+              <p>{user.age}</p>
             </div>
-            <div className="questions-content">
-              <h2>Question 2</h2>
-            </div>
-            <div className="questions-content">
-              <h2>Question 3</h2>
-            </div>
-           
-           
-            
+          )
+         })}
         </div>
-        
-    </div> );
-}
+      </div>
+    </div>  
+)}
 
 export default TestsContent;
