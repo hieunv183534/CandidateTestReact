5f0cef3a2136753cc63c51fd5d33de00899beccf
your name <abc@gmail.com>
2022-06-23 21:11:07 +0700

    a

--- src/component/content/QuestionsContent.js
+++ src/component/content/QuestionsContent.js
@@ -10,6 +10,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 import { Form } from 'devextreme-react/form';
 import PopupConfirm from '../common/PopupConfirm.js';
 import { toast } from 'react-toastify';
+import Dropdown from '../common/Dropdown.js';
 
 function QuestionsContent() {
     const [reload, setReload] = useState(true);
@@ -18,6 +19,10 @@ function QuestionsContent() {
     const [showFormPopup, setShowFormPopup] = useState(false);
     const [formTitle, setFormTitle] = useState("Thêm tài khoản");
     const [popupConfirmSetup, setPopupConfirmSetup] = useState({ isOpen: false, actions: [] });
+    let filters = {
+        type: 1,
+        category: 1
+    };
     const questionEmpty = {
         type: 3,
         contentText: '',
@@ -30,38 +35,44 @@ function QuestionsContent() {
         "category"
     ];
 
-    useEffect(() => {
-        QuestionApi.getListQuestion(100, 0, 3, 1)
+
+
+    const loadQuestions = () => {
+        QuestionApi.getListQuestion(100, 0, filters.type, filters.category)
             .then((res) => {
                 res.data.data.data.forEach(q => {
                     q = completeQuestion(q);
                 });
                 setQuestions(res.data.data.data);
-                console.log(res.data.data);
+                // console.log(res.data.data);
             })
             .catch((err) => {
                 console.error(err);
             });
+    }
+
+    useEffect(() => {
+        loadQuestions();
     }, [reload]);
 
     const completeQuestion = (question) => {
         switch (question.type) {
-            case 1: question.type = 'Trac nghiem 1 dap an'; break;
-            case 2: question.type = 'Trac nghiem nhieu dap an'; break;
-            case 3: question.type = 'Tu luan'; break;
+            case 1: question.type = 'Trắc nghiệm 1 đáp án'; break;
+            case 2: question.type = 'Trắc nghiệm nhiều đáp án'; break;
+            case 3: question.type = 'Tự luận'; break;
         }
 
 
         switch (question.category) {
             case 1: question.category = 'Gmat'; break;
-            case 2: question.category = 'Tieng anh'; break;
-            case 3: question.category = 'Chuyen mon'; break;
+            case 2: question.category = 'Tiếng anh'; break;
+            case 3: question.category = 'Chuyên môn'; break;
         }
         return question;
     }
 
     const addQuestionOnClick = () => {
-        setFormTitle("Thêm caau hoir");
+        setFormTitle("Thêm câu hỏi");
         setQuestion(questionEmpty);
         setShowFormPopup(true);
     }
@@ -75,7 +86,7 @@ function QuestionsContent() {
         setId(data.data.id);
         setPopupConfirmSetup({
             title: "Thông báo",
-            content: "Bạn muốn làm gì với caau hoir " + data.data.contentText,
+            content: "Bạn muốn làm gì với câu hỏi" + data.data.contentText,
             isOpen: true,
             actions:
                 [
@@ -90,7 +101,7 @@ function QuestionsContent() {
                         text: "Chỉnh sửa",
                         buttonType: "btn-primary",
                         callBack: () => {
-                            setFormTitle("Chỉnh sửa Caau hoir");
+                            setFormTitle("Chỉnh sửa câu hỏi");
                             setShowFormPopup(true);
                             setPopupConfirmSetup({ ...popupConfirmSetup, isOpen: false })
                         }
@@ -101,10 +112,10 @@ function QuestionsContent() {
                         callBack: () => {
                             QuestionApi.delete(data.data.id).then(res => {
                                 setReload(!reload);
-                                toast.success('Xóa caau hoir thành công');
+                                toast.success('Xóa câu hỏi thành công');
                                 setPopupConfirmSetup({ ...popupConfirmSetup, isOpen: false })
                             }).catch(err => {
-                                toast.error('Xóa caau hoi thất bại');
+                                toast.error('Xóa câu hỏi thất bại');
                                 setPopupConfirmSetup({ ...popupConfirmSetup, isOpen: false })
                             });
                         }
@@ -115,25 +126,37 @@ function QuestionsContent() {
 
     const formQuestionOnSubmit = () => {
         console.log(formTitle, question);
-        if (formTitle == "Thêm caau hoir") {
+        if (formTitle == "Thêm câu hỏi") {
             QuestionApi.add(question).then(res => {
                 setReload(!reload);
-                toast.success('Thêm caau hoir thành công');
+                toast.success('Thêm câu hỏi thành công');
                 setShowFormPopup(false);
             }).catch(err => {
-                toast.error('Thêm caau hoir thất bại');
+                toast.error('Thêm câu hỏi thất bại');
                 setShowFormPopup(false);
             })
         } else {
             QuestionApi.update(id, question).then(res => {
                 setReload(!reload);
-                toast.success('Cập nhật caau hoir thành công');
+                toast.success('Cập nhật câu hỏi thành công');
                 setShowFormPopup(false);
             }).catch(err => {
-                toast.error('Cập nhật cau hoi thất bại');
+                toast.error('Cập nhật câu hỏi thất bại');
                 setShowFormPopup(false);
             })
         }
+    }
+
+    const actions = [
+        { id: 1, text1: "Trắc nghiệm 1 đáp án" },
+        { id: 2, text1: "Trắc nghiệm nhiều đáp án" },
+        { id: 3, text1: "Tự luận" }
+    ];
+
+    const dropdownOnSelect = (item) => {
+        filters.type = item.id;
+        console.log(filters);
+        loadQuestions();
     }
 
 
@@ -141,7 +164,8 @@ function QuestionsContent() {
     return (
         <div className="table-account">
             <div className="header-table">
-                <Button btnText={"Thêm tài khoản"} btnType={"btn-primary"} btnOnClick={addQuestionOnClick} />
+                <Dropdown actions={actions} onSelect={dropdownOnSelect} />
+                <Button btnText={"Thêm câu hỏi"} btnType={"btn-primary"} btnOnClick={addQuestionOnClick} />
             </div>
             <Table rows={questions} columns={columns} onRowDblClick={questionAction} />
             <Dialog open={showFormPopup} onClose={() => setShowFormPopup(false)}>
